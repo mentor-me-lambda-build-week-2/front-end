45d0c69adc532c3ca465da88336e1a745724cac4
tactnician <tactnician@gmail.com>
2019-03-18 14:43:20 -0400

    added Login and Dashboard components

---
+++ src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Route, Redirect, withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+
+export default function Dashboard() {
+    return (
+        <div>
+        
+        </div>
+    )
+}
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
+
+export default class Login extends Component {
+    render() {
+        return (
+        <div>
+            
+        </div>
+        )
+    }
+}
